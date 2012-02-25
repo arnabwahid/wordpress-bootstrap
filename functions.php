bd63ffdc07ef24d60727b7b68bd3cdc49dff8b1b
Chris Barnes <barnes.chris@gmail.com>
2012-02-25 15:13:53 -0500

    Add label class to tag cloud widget.
    Still need to add the appropriate label class to perform tag cloud
    coloring in php as opposed to javascript.

--- functions.php
+++ functions.php
@@ -173,6 +173,21 @@ function my_widget_tag_cloud_args( $args ) {
 	return $args;
 }
 
+
+
+// filter tag clould output so that it can be styled by CSS
+function add_tag_class( $taglinks ) {
+    $tags = explode('</a>', $taglinks);
+    $regex = "#(.*tag-link[-])(.*)(' title.*)#e";
+        foreach( $tags as $tag ) {
+        	$tagn[] = preg_replace($regex, "('$1$2 label tag-'.get_tag($2)->slug.'$3')", $tag );
+        }
+    $taglinks = implode('</a>', $tagn);
+    return $taglinks;
+}
+
+add_action('wp_tag_cloud', 'add_tag_class');
+
 add_filter('wp_tag_cloud','wp_tag_cloud_filter', 10, 2);
 
 function wp_tag_cloud_filter($return, $args)
