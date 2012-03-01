2dafa112b6b559a3775a9f05920bd1ba33d9996a
Cooper Marcus <cooper@Coopers-MacBook-Air.local>
2012-02-29 16:51:02 -0800

    And a little HR to make things pretty

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer role="contentinfo">
 			
 				<div id="inner-footer" class="clearfix">
-
+          <hr />
           <div id="widget-footer" class="clearfix row-fluid">
             <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
             <?php endif; ?>
