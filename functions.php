95ba0c39731b6b354bb1030c5ee589c590d75d3e
Joel Davis <jlnd-git@yellowbutton.com>
2012-05-15 12:38:19 -0700

    Only apply active class to top level menu items.

--- functions.php
+++ functions.php
@@ -441,7 +441,7 @@ add_editor_style('editor-style.css');
 
 add_filter('nav_menu_css_class', 'add_active_class', 10, 2 );
 function add_active_class($classes, $item) {
-	if(in_array('current-menu-item', $classes)) {
+	if($item->menu_item_parent == 0 && in_array('current-menu-item', $classes)) {
         $classes[] = "active";
 	}
     return $classes;
