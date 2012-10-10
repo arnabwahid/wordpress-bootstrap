37ac7479da50bfb43248deb27c54155a3a9c13f4
chrisbarnes <barnes.chris@gmail.com>
2012-10-09 21:34:24 -0400

    Added 'lead' class to first paragraph of content.
    Doesn't add it on the page-homepage template.

--- functions.php
+++ functions.php
@@ -373,6 +373,18 @@ function add_class_attachment_link($html){
 }
 add_filter('wp_get_attachment_link','add_class_attachment_link',10,1);
 
+// Add lead class to first paragraph
+function first_paragraph($content){
+    global $post;
+
+    // if we're on the homepage, don't add the lead class to the first paragraph of text
+    if( is_page_template( 'page-homepage.php' ) )
+        return $content;
+    else
+        return preg_replace('/<p([^>]+)?>/', '<p$1 class="lead">', $content, 1);
+}
+add_filter('the_content', 'first_paragraph');
+
 // Menu output mods
 class description_walker extends Walker_Nav_Menu
 {
