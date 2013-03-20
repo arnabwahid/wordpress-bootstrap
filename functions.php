a3f05073873e9e40d8398160bc9e919244734a60
Dean Taylor <deanmarktaylor@gmail.com>
2013-03-20 12:46:00 +0000

    PHP Strict Only variables should be passed by ref

--- functions.php
+++ functions.php
@@ -177,7 +177,7 @@ add_filter('get_comments_number', 'comment_count', 0);
 function comment_count( $count ) {
 	if ( ! is_admin() ) {
 		global $id;
-	    $comments_by_type = &separate_comments(get_comments('status=approve&post_id=' . $id));
+	    $comments_by_type = separate_comments(get_comments('status=approve&post_id=' . $id));
 	    return count($comments_by_type['comment']);
 	} else {
 	    return $count;
