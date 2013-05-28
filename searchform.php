f4b8117a530b7bd06e303f6dee089f847b725dc1
Noah Bass <n.bass@me.com>
2013-05-28 14:17:54 -0400

    Adding new form classes to the searchform

--- searchform.php
+++ searchform.php
@@ -1,9 +1,16 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-stacked">
-    <fieldset>
+<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
+	<fieldset>
 		<div class="clearfix">
-			<div class="input-append input-prepend">
-				<span class="add-on"><i class="icon-search"></i></span><input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" /><button type="submit" class="btn btn-primary"><?php _e("Search","bonestheme"); ?></button>
+			<div class="input-group col col-lg-7">
+				<span class="input-group-addon">
+					<span class="glyphicon glyphicon-search"></span>
+				</span>
+				<input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />
+				<!--<button type="submit" class="btn btn-primary"><?php _e("Search","bonestheme"); ?></button>-->
+				<span class="input-group-btn">
+					<button type="submit" class="btn btn-primary" type="button"><?php _e("Search","bonestheme"); ?></button>
+				</span>
 			</div>
-        </div>
-    </fieldset>
+		</div>
+	</fieldset>
 </form>
