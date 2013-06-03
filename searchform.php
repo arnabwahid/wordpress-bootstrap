2d6047350c705b489198d308ee3567c5f817c0eb
Noah Bass <n.bass@me.com>
2013-06-03 17:03:57 -0400

    Fixed up the searchform with the correct classes

--- searchform.php
+++ searchform.php
@@ -1,16 +1,12 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
-	<fieldset>
-		<div class="clearfix">
-			<div class="input-group col col-lg-7">
-				<span class="input-group-addon">
-					<span class="glyphicon glyphicon-search"></span>
-				</span>
-				<input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />
-				<!--<button type="submit" class="btn btn-primary"><?php _e("Search","bonestheme"); ?></button>-->
-				<span class="input-group-btn">
-					<button type="submit" class="btn btn-primary" type="button"><?php _e("Search","bonestheme"); ?></button>
-				</span>
-			</div>
+<div class="row form-inline">
+	<form action="<?php echo home_url( '/' ); ?>" method="get" class="col col-lg-12">
+		<div class="input-group">
+			<span class="input-group-addon">
+				<span class="glyphicon glyphicon-search"></span></span>
+			<input type="text" name="s" id="search" placeholder="<?php _e("Search for anything","bonestheme"); ?>" value="<?php the_search_query(); ?>" />
+			<span class="input-group-btn">
+				<button class="btn btn-primary" type="submit"><?php _e("Search","bonestheme"); ?></button>
+			</span>
 		</div>
-	</fieldset>
-</form>
+	</form>
+</div>
