7902dd5ac71c22b49ccba56c762d4b9fa298d37e
barneschris <barnes.chris@gmail.com>
2013-07-24 22:42:43 -0400

    Navbar tweaks, moved search inside collapsable section of navbar, adjusted search form to use 3.0 classes.

--- header.php
+++ header.php
@@ -43,25 +43,25 @@
 			<div class="navbar navbar-fixed-top">
 				<div class="container">
           
-          <a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-          
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-          
-					<!--<a class="brand" id="logo" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>-->
-          
+
+					<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+
 					<div class="nav-collapse collapse navbar-responsive-collapse">
 						<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+
+						<?php if(of_get_option('search_bar', '1')) {?>
+						<form class="navbar-form pull-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+							<input name="s" id="s" type="text" class="search-query" autocomplete="off" placeholder="<?php _e('Search','bonestheme'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
+						</form>
+						<?php } ?>
 					</div>
 					
-					<?php if(of_get_option('search_bar', '1')) {?>
-					<form class="navbar-search pull-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-						<input name="s" id="s" type="text" class="search-query" autocomplete="off" placeholder="<?php _e('Search','bonestheme'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
-					</form>
-					<?php } ?>
+					
 					
 				</div> <!-- end .container -->
 			</div> <!-- end .navbar -->
--- searchform.php
+++ searchform.php
@@ -1,9 +1,10 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-stacked">
+<form action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline">
     <fieldset>
-		<div class="clearfix">
-			<div class="input-append input-prepend">
-				<span class="add-on"><i class="icon-search"></i></span><input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" /><button type="submit" class="btn btn-primary"><?php _e("Search","bonestheme"); ?></button>
-			</div>
-        </div>
+		<div class="input-group col col-lg-12">
+			<input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />
+			<span class="input-group-btn">
+				<button type="submit" class="btn btn-default"><?php _e("Search","bonestheme"); ?></button>
+			</span>
+		</div>
     </fieldset>
 </form>
