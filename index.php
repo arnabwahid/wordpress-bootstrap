157e429debf3ec24a1e4f81a49f5309a62e41c09
Callum Macdonald <github.com@callum-macdonald.com>
2013-08-21 13:29:26 +0200

    Use __() instead of _e()
    The `next_posts_link()` and `previous-posts_link()` want the translated value returned, not output, so we want `__()` instead of `_e()`.

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
 					<?php } else { // if it is disabled, display regular wp prev & next links ?>
 						<nav class="wp-prev-next">
 							<ul class="clearfix">
-								<li class="prev-link"><?php next_posts_link(_e('&laquo; Older Entries', "bonestheme")) ?></li>
-								<li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', "bonestheme")) ?></li>
+								<li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "bonestheme")) ?></li>
+								<li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "bonestheme")) ?></li>
 							</ul>
 						</nav>
 					<?php } ?>		
