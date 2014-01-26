4821f522e56a1e36660e468612d9cbddaf2261c8
barneschris <barnes.chris@gmail.com>
2014-01-26 15:17:06 -0500

    Make child theme friendly.

--- functions.php
+++ functions.php
@@ -461,6 +461,10 @@ if( !function_exists("theme_styles") ) {
         // This is the compiled css file from LESS - this means you compile the LESS file locally and put it in the appropriate directory if you want to make any changes to the master bootstrap.css.
         wp_register_style( 'bootstrap', get_template_directory_uri() . '/library/css/bootstrap.css', array(), '1.0', 'all' );
         wp_enqueue_style( 'bootstrap' );
+
+        // For child themes
+        wp_register_style( 'wpbs-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all' );
+        wp_enqueue_style( 'wpbs-style' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
