78f2d0e6d4f8168f26dd6eb41d02b5fd122db05c
Martin Meyerhoff <mamhoff@googlemail.com>
2014-02-04 08:47:49 +0100

    fix wp_title() so it behaves just like the standard wp themes

--- functions.php
+++ functions.php
@@ -484,4 +484,30 @@ if( !function_exists( "theme_js" ) ) {
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
+// Get <head> <title> to behave like other themes
+function wp_bootstrap_wp_title( $title, $sep ) {
+  global $paged, $page;
+
+  if ( is_feed() ) {
+    return $title;
+  }
+
+  // Add the site name.
+  $title .= get_bloginfo( 'name' );
+
+  // Add the site description for the home/front page.
+  $site_description = get_bloginfo( 'description', 'display' );
+  if ( $site_description && ( is_home() || is_front_page() ) ) {
+    $title = "$title $sep $site_description";
+  }
+
+  // Add a page number if necessary.
+  if ( $paged >= 2 || $page >= 2 ) {
+    $title = "$title $sep " . sprintf( __( 'Page %s', 'wpbootstrap' ), max( $paged, $page ) );
+  }
+
+  return $title;
+}
+add_filter( 'wp_title', 'wp_bootstrap_wp_title', 10, 2 );
+
 ?>
