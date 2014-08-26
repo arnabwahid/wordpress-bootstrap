deaba8c507ecb00053ec4b9eca584a7be8fcd3b5
Dejan Noveski <dr.mote@gmail.com>
2014-08-26 15:37:48 +0200

    Fixed the footer to be outside main container so it can be 100% outside

--- footer.php
+++ footer.php
@@ -1,26 +1,20 @@
-			<footer role="contentinfo">
-				<div id="inner-footer" class="<?php if (get_post_meta( $post->ID, '_layout_width_meta', '' )[0] != "container") echo "container";?> clearfix">
-		          <hr />
+		</div> <!-- end #container -->
+			<footer id="mainfooter" role="contentinfo">
+			<div id="inner-footer" class="container clearfix">
 		          <div id="widget-footer" class="clearfix row">
-		          	<?php for ($i=1; $i <= get_theme_mod('footer_widget_areas'); $i++){ ?>
+		            <?php for ($i=1; $i <= get_theme_mod('footer_widget_areas'); $i++){ ?>
 			            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("footer$i") ) : ?>
 			            <?php endif; ?>
 		            <?php }?>
 		          </div>
-					
 					<nav class="clearfix">
 						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
-					
-					<p class="pull-right"><a href="http://320press.com" id="credit320" title="By the dudes of 320press">320press</a></p>
-			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
-				
 				</div> <!-- end #inner-footer -->
 				
 			</footer> <!-- end footer -->
 		
-		</div> <!-- end #container -->
+		
 				
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
