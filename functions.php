8532fb56b603be60bf1e30cc9636e177d53ac7e5
barneschris <barnes.chris@gmail.com>
2014-10-11 14:43:24 -0400

    Some functions cleanup and consolidation.

--- functions.php
+++ functions.php
@@ -1,15 +1,115 @@
 <?php
-/*
-Author: Eddie Machado
-URL: htp://themble.com/bones/
 
-This is where you can drop your custom functions or
-just edit things like thumbnail sizes, header images, 
-sidebars, comments, ect.
-*/
+// Add Translation Option
+load_theme_textdomain( 'wpbootstrap', TEMPLATEPATH.'/languages' );
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if ( is_readable( $locale_file ) ) require_once( $locale_file );
 
-// Get Bones Core Up & Running!
-require_once('library/bones.php');            // core functions (don't remove)
+// Clean up the WordPress Head
+if( !function_exists( "wp_bootstrap_head_cleanup" ) ) {  
+  function wp_bootstrap_head_cleanup() {
+    // remove header links
+    remove_action( 'wp_head', 'feed_links_extra', 3 );                    // Category Feeds
+    remove_action( 'wp_head', 'feed_links', 2 );                          // Post and Comment Feeds
+    remove_action( 'wp_head', 'rsd_link' );                               // EditURI link
+    remove_action( 'wp_head', 'wlwmanifest_link' );                       // Windows Live Writer
+    remove_action( 'wp_head', 'index_rel_link' );                         // index link
+    remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );            // previous link
+    remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );             // start link
+    remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for Adjacent Posts
+    remove_action( 'wp_head', 'wp_generator' );                           // WP version
+  }
+}
+// Launch operation cleanup
+add_action( 'init', 'wp_bootstrap_head_cleanup' );
+
+// remove WP version from RSS
+if( !function_exists( "wp_bootstrap_rss_version" ) ) {  
+  function wp_bootstrap_rss_version() { return ''; }
+}
+add_filter( 'the_generator', 'wp_bootstrap_rss_version' );
+
+// Remove the […] in a Read More link
+if( !function_exists( "wp_bootstrap_excerpt_more" ) ) {  
+  function wp_bootstrap_excerpt_more( $more ) {
+    global $post;
+    return '...  <a href="'. get_permalink($post->ID) . '" class="more-link" title="Read '.get_the_title($post->ID).'">Read more &raquo;</a>';
+  }
+}
+add_filter('excerpt_more', 'wp_bootstrap_excerpt_more');
+
+// Add WP 3+ Functions & Theme Support
+if( !function_exists( "wp_bootstrap_theme_support" ) ) {  
+  function wp_bootstrap_theme_support() {
+    add_theme_support( 'post-thumbnails' );      // wp thumbnails (sizes handled in functions.php)
+    set_post_thumbnail_size( 125, 125, true );   // default thumb size
+    add_theme_support( 'custom-background' );  // wp custom background
+    add_theme_support( 'automatic-feed-links' ); // rss
+
+    // Add post format support - if these are not needed, comment them out
+    add_theme_support( 'post-formats',      // post formats
+      array( 
+        'aside',   // title less blurb
+        'gallery', // gallery of images
+        'link',    // quick link to other site
+        'image',   // an image
+        'quote',   // a quick quote
+        'status',  // a Facebook like status update
+        'video',   // video 
+        'audio',   // audio
+        'chat'     // chat transcript 
+      )
+    );  
+
+    add_theme_support( 'menus' );            // wp menus
+    
+    register_nav_menus(                      // wp3+ menus
+      array( 
+        'main_nav' => 'The Main Menu',   // main nav in header
+        'footer_links' => 'Footer Links' // secondary nav in footer
+      )
+    );  
+  }
+}
+// launching this stuff after theme setup
+add_action( 'after_setup_theme','wp_bootstrap_theme_support' );
+
+function wp_bootstrap_main_nav() {
+  // Display the WordPress menu if available
+  wp_nav_menu( 
+    array( 
+      'menu' => 'main_nav', /* menu name */
+      'menu_class' => 'nav navbar-nav',
+      'theme_location' => 'main_nav', /* where in the theme it's assigned */
+      'container' => 'false', /* container class */
+      'fallback_cb' => 'wp_bootstrap_main_nav_fallback', /* menu fallback */
+      'walker' => new Bootstrap_walker()
+    )
+  );
+}
+
+function wp_bootstrap_footer_links() { 
+  // Display the WordPress menu if available
+  wp_nav_menu(
+    array(
+      'menu' => 'footer_links', /* menu name */
+      'theme_location' => 'footer_links', /* where in the theme it's assigned */
+      'container_class' => 'footer-links clearfix', /* container class */
+      'fallback_cb' => 'wp_bootstrap_footer_links_fallback' /* menu fallback */
+    )
+  );
+}
+
+// this is the fallback for header menu
+function wp_bootstrap_main_nav_fallback() { 
+  /* you can put a default here if you like */ 
+}
+
+// this is the fallback for footer menu
+function wp_bootstrap_footer_links_fallback() { 
+  /* you can put a default here if you like */ 
+}
 
 // Shortcodes
 require_once('library/shortcodes.php');
@@ -60,69 +160,70 @@ you like. Enjoy!
 
 // Sidebars & Widgetizes Areas
 function wp_bootstrap_register_sidebars() {
-    register_sidebar(array(
-    	'id' => 'sidebar1',
-    	'name' => 'Main Sidebar',
-    	'description' => 'Used on every page BUT the homepage page template.',
-    	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    	'after_widget' => '</div>',
-    	'before_title' => '<h4 class="widgettitle">',
-    	'after_title' => '</h4>',
-    ));
+  register_sidebar(array(
+  	'id' => 'sidebar1',
+  	'name' => 'Main Sidebar',
+  	'description' => 'Used on every page BUT the homepage page template.',
+  	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+  	'after_widget' => '</div>',
+  	'before_title' => '<h4 class="widgettitle">',
+  	'after_title' => '</h4>',
+  ));
     
-    register_sidebar(array(
-    	'id' => 'sidebar2',
-    	'name' => 'Homepage Sidebar',
-    	'description' => 'Used only on the homepage page template.',
-    	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    	'after_widget' => '</div>',
-    	'before_title' => '<h4 class="widgettitle">',
-    	'after_title' => '</h4>',
-    ));
+  register_sidebar(array(
+  	'id' => 'sidebar2',
+  	'name' => 'Homepage Sidebar',
+  	'description' => 'Used only on the homepage page template.',
+  	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+  	'after_widget' => '</div>',
+  	'before_title' => '<h4 class="widgettitle">',
+  	'after_title' => '</h4>',
+  ));
     
-    register_sidebar(array(
-      'id' => 'footer1',
-      'name' => 'Footer 1',
-      'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
-      'after_widget' => '</div>',
-      'before_title' => '<h4 class="widgettitle">',
-      'after_title' => '</h4>',
-    ));
+  register_sidebar(array(
+    'id' => 'footer1',
+    'name' => 'Footer 1',
+    'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h4 class="widgettitle">',
+    'after_title' => '</h4>',
+  ));
 
-    register_sidebar(array(
-      'id' => 'footer2',
-      'name' => 'Footer 2',
-      'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
-      'after_widget' => '</div>',
-      'before_title' => '<h4 class="widgettitle">',
-      'after_title' => '</h4>',
-    ));
+  register_sidebar(array(
+    'id' => 'footer2',
+    'name' => 'Footer 2',
+    'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h4 class="widgettitle">',
+    'after_title' => '</h4>',
+  ));
 
-    register_sidebar(array(
-      'id' => 'footer3',
-      'name' => 'Footer 3',
-      'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
-      'after_widget' => '</div>',
-      'before_title' => '<h4 class="widgettitle">',
-      'after_title' => '</h4>',
-    ));
+  register_sidebar(array(
+    'id' => 'footer3',
+    'name' => 'Footer 3',
+    'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h4 class="widgettitle">',
+    'after_title' => '</h4>',
+  ));
     
     
-    /* 
-    to add more sidebars or widgetized areas, just copy
-    and edit the above sidebar code. In order to call 
-    your new sidebar just use the following code:
-    
-    Just change the name to whatever your new
-    sidebar's id is, for example:
-    
-    To call the sidebar in your template, you can just copy
-    the sidebar.php file and rename it to your sidebar's name.
-    So using the above example, it would be:
-    sidebar-sidebar2.php
-    
-    */
+  /* 
+  to add more sidebars or widgetized areas, just copy
+  and edit the above sidebar code. In order to call 
+  your new sidebar just use the following code:
+  
+  Just change the name to whatever your new
+  sidebar's id is, for example:
+  
+  To call the sidebar in your template, you can just copy
+  the sidebar.php file and rename it to your sidebar's name.
+  So using the above example, it would be:
+  sidebar-sidebar2.php
+  
+  */
 } // don't remove this bracket!
+add_action( 'widgets_init', 'wp_bootstrap_register_sidebars' );
 
 /************* COMMENT LAYOUT *********************/
 		
@@ -432,9 +533,6 @@ class Bootstrap_walker extends Walker_Nav_Menu{
 
 add_editor_style('editor-style.css');
 
-// Add Twitter Bootstrap's standard 'active' class name to the active nav link item
-add_filter('nav_menu_css_class', 'add_active_class', 10, 2 );
-
 function add_active_class($classes, $item) {
 	if( $item->menu_item_parent == 0 && in_array('current-menu-item', $classes) ) {
     $classes[] = "active";
@@ -442,6 +540,9 @@ function add_active_class($classes, $item) {
   
   return $classes;
 }
+
+// Add Twitter Bootstrap's standard 'active' class name to the active nav link item
+add_filter('nav_menu_css_class', 'add_active_class', 10, 2 );
 
 // enqueue styles
 if( !function_exists("theme_styles") ) {  
@@ -460,6 +561,11 @@ add_action( 'wp_enqueue_scripts', 'theme_styles' );
 // enqueue javascript
 if( !function_exists( "theme_js" ) ) {  
   function theme_js(){
+
+    if ( !is_admin() ){
+      if ( is_singular() AND comments_open() AND ( get_option( 'thread_comments' ) == 1) ) 
+        wp_enqueue_script( 'comment-reply' );
+    }
   
     wp_register_script( 'bootstrap', 
       get_template_directory_uri() . '/library/js/bootstrap.min.js', 
@@ -509,5 +615,95 @@ function wp_bootstrap_wp_title( $title, $sep ) {
   return $title;
 }
 add_filter( 'wp_title', 'wp_bootstrap_wp_title', 10, 2 );
+
+// Related Posts Function (call using wp_bootstrap_related_posts(); )
+function wp_bootstrap_related_posts() {
+  echo '<ul id="bones-related-posts">';
+  global $post;
+  $tags = wp_get_post_tags($post->ID);
+  if($tags) {
+    foreach($tags as $tag) { $tag_arr .= $tag->slug . ','; }
+        $args = array(
+          'tag' => $tag_arr,
+          'numberposts' => 5, /* you can change this to show more */
+          'post__not_in' => array($post->ID)
+      );
+        $related_posts = get_posts($args);
+        if($related_posts) {
+          foreach ($related_posts as $post) : setup_postdata($post); ?>
+              <li class="related_post"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+          <?php endforeach; } 
+      else { ?>
+            <li class="no_related_post">No Related Posts Yet!</li>
+    <?php }
+  }
+  wp_reset_query();
+  echo '</ul>';
+}
+
+// Numeric Page Navi (built into the theme by default)
+function page_navi($before = '', $after = '') {
+  global $wpdb, $wp_query;
+  $request = $wp_query->request;
+  $posts_per_page = intval(get_query_var('posts_per_page'));
+  $paged = intval(get_query_var('paged'));
+  $numposts = $wp_query->found_posts;
+  $max_page = $wp_query->max_num_pages;
+  if ( $numposts <= $posts_per_page ) { return; }
+  if(empty($paged) || $paged == 0) {
+    $paged = 1;
+  }
+  $pages_to_show = 7;
+  $pages_to_show_minus_1 = $pages_to_show-1;
+  $half_page_start = floor($pages_to_show_minus_1/2);
+  $half_page_end = ceil($pages_to_show_minus_1/2);
+  $start_page = $paged - $half_page_start;
+  if($start_page <= 0) {
+    $start_page = 1;
+  }
+  $end_page = $paged + $half_page_end;
+  if(($end_page - $start_page) != $pages_to_show_minus_1) {
+    $end_page = $start_page + $pages_to_show_minus_1;
+  }
+  if($end_page > $max_page) {
+    $start_page = $max_page - $pages_to_show_minus_1;
+    $end_page = $max_page;
+  }
+  if($start_page <= 0) {
+    $start_page = 1;
+  }
+    
+  echo $before.'<ul class="pagination">'."";
+  if ($paged > 1) {
+    $first_page_text = "&laquo";
+    echo '<li class="prev"><a href="'.get_pagenum_link().'" title="First">'.$first_page_text.'</a></li>';
+  }
+    
+  $prevposts = get_previous_posts_link('&larr; Previous');
+  if($prevposts) { echo '<li>' . $prevposts  . '</li>'; }
+  else { echo '<li class="disabled"><a href="#">&larr; Previous</a></li>'; }
+  
+  for($i = $start_page; $i  <= $end_page; $i++) {
+    if($i == $paged) {
+      echo '<li class="active"><a href="#">'.$i.'</a></li>';
+    } else {
+      echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
+    }
+  }
+  echo '<li class="">';
+  next_posts_link('Next &rarr;');
+  echo '</li>';
+  if ($end_page < $max_page) {
+    $last_page_text = "&raquo;";
+    echo '<li class="next"><a href="'.get_pagenum_link($max_page).'" title="Last">'.$last_page_text.'</a></li>';
+  }
+  echo '</ul>'.$after."";
+}
+
+// Remove <p> tags from around images
+function filter_ptags_on_images( $content ){
+  return preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content );
+}
+add_filter( 'the_content', 'filter_ptags_on_images' );
 
 ?>
