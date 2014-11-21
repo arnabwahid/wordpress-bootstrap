5b4e6860831ddd7f4c092aa827333f8f602350b1
barneschris <barnes.chris@gmail.com>
2014-11-20 22:19:18 -0500

    Page nav localizations.

--- functions.php
+++ functions.php
@@ -677,12 +677,12 @@ function page_navi($before = '', $after = '') {
   echo $before.'<ul class="pagination">'."";
   if ($paged > 1) {
     $first_page_text = "&laquo";
-    echo '<li class="prev"><a href="'.get_pagenum_link().'" title="First">'.$first_page_text.'</a></li>';
+    echo '<li class="prev"><a href="'.get_pagenum_link().'" title="' . __('First','wpbootstrap') . '">'.$first_page_text.'</a></li>';
   }
     
-  $prevposts = get_previous_posts_link('&larr; Previous');
+  $prevposts = get_previous_posts_link( __('&larr; Previous','wpbootstrap') );
   if($prevposts) { echo '<li>' . $prevposts  . '</li>'; }
-  else { echo '<li class="disabled"><a href="#">&larr; Previous</a></li>'; }
+  else { echo '<li class="disabled"><a href="#">' . __('&larr; Previous','wpbootstrap') . '</a></li>'; }
   
   for($i = $start_page; $i  <= $end_page; $i++) {
     if($i == $paged) {
@@ -692,11 +692,11 @@ function page_navi($before = '', $after = '') {
     }
   }
   echo '<li class="">';
-  next_posts_link('Next &rarr;');
+  next_posts_link( __('Next &rarr;','wpbootstrap') );
   echo '</li>';
   if ($end_page < $max_page) {
     $last_page_text = "&raquo;";
-    echo '<li class="next"><a href="'.get_pagenum_link($max_page).'" title="Last">'.$last_page_text.'</a></li>';
+    echo '<li class="next"><a href="'.get_pagenum_link($max_page).'" title="' . __('Last','wpbootstrap') . '">'.$last_page_text.'</a></li>';
   }
   echo '</ul>'.$after."";
 }
